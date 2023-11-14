592a0cd3c86a8829209332dbd59441e2c69a2f97
Planting12 <simej1214@gmail.com>
2023-11-14 22:31:37 +0900

    🌈 style: 주석 처리된 코드 수정

--- vendingMachine/js/classes/vendingMachineEvents.js
+++ vendingMachine/js/classes/vendingMachineEvents.js
@@ -39,19 +39,17 @@ class VendingMachineEvents {
      * 1) 소지금 === 소지금 - 입금액
      * 2) 잔액 === 기존 잔액 + 입금액
      * 3) 입금액이 소지금보다 많으면 경고창 출력
-     * 4) 입금액이 정상적으로 입금되면 입금창은 초기화 되야한다.
+     * 4) 입금액이 정상적으로 입금되면 입금창 초기화
      */
     this.btnPut.addEventListener("click", () => {
       // 입력값
       const inputCost = parseInt(this.inputCostEl.value);
       // 소지금
       const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(",", ""));
-
       // 잔액
       const balanceVal = parseInt(this.balance.textContent.replaceAll(",", ""));
 
       if (inputCost) {
-        // 입금액이 소지금 보다 적거나 같다면
         if (inputCost <= myMoneyVal && inputCost > 0) {
           this.myMoney.textContent =
             new Intl.NumberFormat().format(myMoneyVal - inputCost) + "원";
@@ -60,8 +58,6 @@ class VendingMachineEvents {
             new Intl.NumberFormat().format(
               (balanceVal ? balanceVal : 0) + inputCost
             ) + "원";
-
-          // 입금액이 소지금보다 많다!
         } else {
           alert("소지금이 부족합니다.");
         }
@@ -72,13 +68,13 @@ class VendingMachineEvents {
 
     /**
      * 2. 거스름돈 반환 버튼
-     * 1) 반환버튼을 누르면 소지금 === 잔액 + 소지금
-     * 2) 반환버튼을 누르면 잔액창이 초기화됩니다.
+     * 반환버튼을 누르면
+     * 1) 소지금 === 잔액 + 소지금
+     * 2) 잔액창 초기화
      */
     this.btnReturn.addEventListener("click", () => {
       // 잔액
       const balanceVal = parseInt(this.balance.textContent.replaceAll(",", ""));
-
       // 소지금
       const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(",", ""));
 
@@ -92,9 +88,9 @@ class VendingMachineEvents {
     /**
      * 3. 자판기 장바구니 채우기
      * 1) 아이템을 누르면 잔액 === 잔액 - 아이템 가격
-     * 2) 아이템 가격이 잔액보다 크다면 경고메세지를 줍니다.
-     * 3) 아이템이 장바구니에 들어갑니다.
-     * 4) 아이템의 count가 -1이 됩니다. 아이템의 카운트가 0이되면 품절 표시를 합니다.
+     * 2) 아이템 가격이 잔액보다 크면 경고메세지 출력
+     * 3) 아이템이 장바구니에 들어가면 아이템의 count가 -1이 됨
+     * 4) 아이템의 카운트가 0이되면 품절 표시
      */
     this.btnsCola = document.querySelectorAll(".section1 .btn-cola");
 
@@ -113,7 +109,7 @@ class VendingMachineEvents {
             new Intl.NumberFormat().format(balanceVal - targetElPrice) + "원";
 
           for (const item of stagedListitem) {
-            // 클릭한 콜라의 이름과 장바구니에 있던 콜라의 이름이 같은지 비교!
+            // 클릭한 콜라 이름과 장바구니에 있던 콜라 이름이 같은지 비교
             if (targetEl.dataset.item === item.dataset.item) {
               // 이미 장바구니에 콜라가 있다면 카운트 +1
               item.querySelector("strong").firstChild.textContent =
@@ -125,12 +121,10 @@ class VendingMachineEvents {
             }
           }
 
-          // 처음 선택했을 경우에만 장바구니에 콜라를 생성합니다.
           if (!isStaged) {
             //장바구니 콜라 생성
             this.stagedItemGenerator(event.currentTarget);
           }
-
           // 자판기 콜라 개수 차감
           targetEl.dataset.count--;
 
@@ -154,8 +148,8 @@ class VendingMachineEvents {
 
     /**
      * 4. 획득 버튼 기능
-     * 1) 장바구니에 있는 음료수 목록이 획득한 음료 목록으로 이동합니다.
-     * 2) 획득한 음료의 모든 금액을 합하여 총 금액을 업데이트합니다.
+     * 1) 장바구니에 있는 음료수 목록이 획득한 음료 목록으로 이동
+     * 2) 획득한 음료의 모든 금액을 합하여 총 금액을 업데이트
      */
 
     this.btnGet.addEventListener("click", () => {
@@ -166,7 +160,7 @@ class VendingMachineEvents {
       let totalPrice = 0;
 
       for (const itemStaged of itemStagedList) {
-        let isGet = false; // 이미 획득했는가?
+        let isGet = false;
         for (const itemGet of itemGetList) {
           console.log(itemStaged.querySelector("strong"));
           // 장바구니의 콜라가 이미 획득한 목록에 있다면
@@ -191,7 +185,7 @@ class VendingMachineEvents {
       // 장바구니 목록 초기화
       this.stagedList.innerHTML = null;
 
-      // 획득한 음료 리스트를 순회하면서 총금액을 계산합니다.
+      // 획득한 음료 리스트를 순회하면서 총금액을 계산
       this.getList.querySelectorAll("li").forEach((itemGet) => {
         totalPrice +=
           parseInt(itemGet.dataset.price) *
